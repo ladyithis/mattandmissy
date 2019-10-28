cbc4b63369998f1824523e969d3ea0dea140b631
Matt Messa <messa.matt@gmail.com>
2019-10-27 17:53:30 -0700

    Nav (#26)
    * added wedding and rsvp components and edited navbar
    
    * nav bar and rsvp code enter

---
+++ MattAndMissy/ClientApp/src/components/rsvp/Home.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+import { RSVPBox } from './RSVPBox';
+
+
+export class RSVPHome extends Component {
+    static displayName = RSVPHome.name;
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Wedding RSVP</h1>
+                <RSVPBox/>
+            </div>
+        );
+    }
+}
